6aaad49a17a5b11cca27ff1036ba6b0f5a970eb9
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-09-17 22:36:46 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -23,7 +23,7 @@ if ! command -v docker &> /dev/null; then
     sudo apt-get update
     sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     sudo apt-get update
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io
     sudo systemctl start docker
