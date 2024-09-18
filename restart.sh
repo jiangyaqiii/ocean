c7275d735607769535e3c13d343f36c7fde5a0af
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-09-18 23:21:04 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,5 +1,12 @@
 cd ocean
-sed -i "s/ALLOWED_ADMINS:.*$/ALLOWED_ADMINS: '[\"123456\"]'/" docker-compose.yml
-sed -i "s/PRIVATE_KEY:.*$/PRIVATE_KEY: '123456'/" docker-compose.yml
+
+sed -i "s/ALLOWED_ADMINS:.*$/ALLOWED_ADMINS: '[\"$allowed_admins\"]'/" docker-compose.yml
+sed -i "s/PRIVATE_KEY:.*$/PRIVATE_KEY: '$private_key'/" docker-compose.yml
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
+docker-compose up -d
+
+echo "节点重启完成！"
+
+cd
+rm restart.sh
