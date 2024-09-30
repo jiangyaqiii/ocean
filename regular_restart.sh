a2cf3ca72f5d9160b80499497bc8cd83b39eb272
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-09-30 16:08:37 +0800

    Create regular_restart.sh

---
+++ regular_restart.sh
@@ -0,0 +1,30 @@
+# ===================================公共模块===监控screen模块======================================================================
+cd ~
+#监控screen脚本
+echo '#!/bin/bash
+while true
+do
+    if ! screen -list | grep -q "Quili"; then
+        echo "Screen session not found, restarting..."
+        cd /root/ceremonyclient/node
+        screen -dmS Quili bash -c "./release_autorun.sh"
+    fi
+    sleep 86400  # 每隔10秒检查一次
+done' > monit.sh
+##给予执行权限
+chmod +x monit.sh
+# ================================================================================================================================
+echo '[Unit]
+Description=Quili Monitor Service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/bin/bash /root/monit.sh
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/quili_monitor.service
+sudo systemctl daemon-reload
+sudo systemctl enable quili_monitor.service
+sudo systemctl start quili_monitor.service
+sudo systemctl status quili_monitor.service
