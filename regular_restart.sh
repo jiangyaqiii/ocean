c53397d328ead6d03574c41f917b06c4b7dff4ba
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-10-12 03:00:56 +0800

    Update regular_restart.sh

--- regular_restart.sh
+++ regular_restart.sh
@@ -9,9 +9,9 @@ do
   docker rm $docker_id
   docker-compose up -d
   sleep 86400
-done' > regular.sh
+done' > regular_ocean.sh
 ##给予执行权限
-chmod +x regular.sh
+chmod +x regular_ocean.sh
 # ================================================================================================================================
 echo ' [Unit]
 Description=Restart Ocean Docker Containers Service
@@ -19,7 +19,7 @@ After=network.target
 
 [Service]
 Type=simple
-ExecStart=/bin/bash /root/regular.sh
+ExecStart=/bin/bash /root/regular_ocean.sh
 
 [Install]
 WantedBy=multi-user.target' > /etc/systemd/system/ocean-restart.service
@@ -27,6 +27,7 @@ sudo systemctl daemon-reload
 sudo systemctl enable ocean-restart.service
 sudo systemctl start ocean-restart.service
 echo '==flag:v2===='
+rm -f regular_restart.sh
 # ================================================flag:v2=====================================================================================
 # echo '[Unit]
 # Description=Timer for restarting Ocean Docker containers every 24 hours
