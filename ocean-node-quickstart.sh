6af864ddd8786fa98f72ded19a3416ba8a328160
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-12-06 18:48:44 +0800

    Update ocean-node-quickstart.sh

--- ocean-node-quickstart.sh
+++ ocean-node-quickstart.sh
@@ -73,30 +73,30 @@ validate_hex "$PRIVATE_KEY"
 # read -p "Please provide the wallet address to be added as Ocean Node admin account: " ALLOWED_ADMINS
 validate_address "$ALLOWED_ADMINS"
 
-echo -ne "Provide the HTTP_API_PORT value or accept the default (press Enter) [\e[1;32m8000\e[0m]: "
-read HTTP_API_PORT
-HTTP_API_PORT=${HTTP_API_PORT:-8000}
-validate_port "$HTTP_API_PORT"
+# echo -ne "Provide the HTTP_API_PORT value or accept the default (press Enter) [\e[1;32m8000\e[0m]: "
+# read HTTP_API_PORT
+# HTTP_API_PORT=${HTTP_API_PORT:-8000}
+# validate_port "$HTTP_API_PORT"
 
-echo -ne "Provide the P2P_ipV4BindTcpPort or accept the default (press Enter) [\e[1;32m9000\e[0m]: "
-read P2P_ipV4BindTcpPort
-P2P_ipV4BindTcpPort=${P2P_ipV4BindTcpPort:-9000}
-validate_port "$P2P_ipV4BindTcpPort"
+# echo -ne "Provide the P2P_ipV4BindTcpPort or accept the default (press Enter) [\e[1;32m9000\e[0m]: "
+# read P2P_ipV4BindTcpPort
+# P2P_ipV4BindTcpPort=${P2P_ipV4BindTcpPort:-9000}
+# validate_port "$P2P_ipV4BindTcpPort"
 
-echo -ne "Provide the P2P_ipV4BindWsPort or accept the default (press Enter) [\e[1;32m9001\e[0m]: "
-read P2P_ipV4BindWsPort
-P2P_ipV4BindWsPort=${P2P_ipV4BindWsPort:-9001}
-validate_port "$P2P_ipV4BindWsPort"
+# echo -ne "Provide the P2P_ipV4BindWsPort or accept the default (press Enter) [\e[1;32m9001\e[0m]: "
+# read P2P_ipV4BindWsPort
+# P2P_ipV4BindWsPort=${P2P_ipV4BindWsPort:-9001}
+# validate_port "$P2P_ipV4BindWsPort"
 
-echo -ne "Provide the P2P_ipV6BindTcpPort or accept the default (press Enter) [\e[1;32m9002\e[0m]: "
-read P2P_ipV6BindTcpPort
-P2P_ipV6BindTcpPort=${P2P_ipV6BindTcpPort:-9002}
-validate_port "$P2P_ipV6BindTcpPort"
+# echo -ne "Provide the P2P_ipV6BindTcpPort or accept the default (press Enter) [\e[1;32m9002\e[0m]: "
+# read P2P_ipV6BindTcpPort
+# P2P_ipV6BindTcpPort=${P2P_ipV6BindTcpPort:-9002}
+# validate_port "$P2P_ipV6BindTcpPort"
 
-echo -ne "Provide the P2P_ipV6BindWsPort or accept the default (press Enter) [\e[1;32m9003\e[0m]: "
-read P2P_ipV6BindWsPort
-P2P_ipV6BindWsPort=${P2P_ipV6BindWsPort:-9003}
-validate_port "$P2P_ipV6BindWsPort"
+# echo -ne "Provide the P2P_ipV6BindWsPort or accept the default (press Enter) [\e[1;32m9003\e[0m]: "
+# read P2P_ipV6BindWsPort
+# P2P_ipV6BindWsPort=${P2P_ipV6BindWsPort:-9003}
+# validate_port "$P2P_ipV6BindWsPort"
 
 # read -p "Provide the public IPv4 address or FQDN where this node will be accessible: " P2P_ANNOUNCE_ADDRESS
 
@@ -128,14 +128,14 @@ services:
     container_name: ocean-node
     restart: on-failure
     ports:
-      - "$HTTP_API_PORT:$HTTP_API_PORT"
-      - "$P2P_ipV4BindTcpPort:$P2P_ipV4BindTcpPort"
-      - "$P2P_ipV4BindWsPort:$P2P_ipV4BindWsPort"
-      - "$P2P_ipV6BindTcpPort:$P2P_ipV6BindTcpPort"
-      - "$P2P_ipV6BindWsPort:$P2P_ipV6BindWsPort"
+      - '8000:8000'
+      - '9010:9010'
+      - '9011:9011'
+      - '9002:9002'
+      - '9003:9003'
     environment:
       PRIVATE_KEY: '$PRIVATE_KEY'
-      RPCS: '{"1":{"rpc":"https://ethereum-rpc.publicnode.com","fallbackRPCs":["https://rpc.ankr.com/eth","https://1rpc.io/eth","https://eth.api.onfinality.io/public"],"chainId":1,"network":"mainnet","chunkSize":100},"10":{"rpc":"https://mainnet.optimism.io","fallbackRPCs":["https://optimism-mainnet.public.blastapi.io","https://rpc.ankr.com/optimism","https://optimism-rpc.publicnode.com"],"chainId":10,"network":"optimism","chunkSize":100},"137":{"rpc":"https://polygon-rpc.com/","fallbackRPCs":["https://polygon-mainnet.public.blastapi.io","https://1rpc.io/matic","https://rpc.ankr.com/polygon"],"chainId":137,"network":"polygon","chunkSize":100},"23294":{"rpc":"https://sapphire.oasis.io","fallbackRPCs":["https://1rpc.io/oasis/sapphire"],"chainId":23294,"network":"sapphire","chunkSize":100},"23295":{"rpc":"https://testnet.sapphire.oasis.io","chainId":23295,"network":"sapphire-testnet","chunkSize":100},"11155111":{"rpc":"https://eth-sepolia.public.blastapi.io","fallbackRPCs":["https://1rpc.io/sepolia","https://eth-sepolia.g.alchemy.com/v2/demo"],"chainId":11155111,"network":"sepolia","chunkSize":100},"11155420":{"rpc":"https://sepolia.optimism.io","fallbackRPCs":["https://endpoints.omniatech.io/v1/op/sepolia/public","https://optimism-sepolia.blockpi.network/v1/rpc/public"],"chainId":11155420,"network":"optimism-sepolia","chunkSize":100}}'
+      RPCS: '{"1":{"rpc":"https://api.securerpc.com/v1","fallbackRPCs":["https://rpc.ankr.com/eth"],"chainId":1,"network":"mainnet","chunkSize":100},"10":{"rpc":"https://mainnet.optimism.io","fallbackRPCs":["https://rpc.ankr.com/optimism"],"chainId":10,"network":"optimism","chunkSize":100},"137":{"rpc":"https://polygon-rpc.com/","fallbackRPCs":["https://rpc.ankr.com/polygon"],"chainId":137,"network":"polygon","chunkSize":100},"23294":{"rpc":"https://sapphire.oasis.io","fallbackRPCs":["https://1rpc.io/oasis/sapphire"],"chainId":23294,"network":"sapphire","chunkSize":100},"23295":{"rpc":"https://testnet.sapphire.oasis.io","chainId":23295,"network":"sapphire-testnet","chunkSize":100},"11155111":{"rpc":"https://eth-sepolia.api.onfinality.io/public","fallbackRPCs":["https://sepolia.gateway.tenderly.co"],"chainId":11155111,"network":"sepolia","chunkSize":100},"11155420":{"rpc":"https://sepolia.optimism.io","fallbackRPCs":["https://endpoints.omniatech.io/v1/op/sepolia/public","https://optimism-sepolia.blockpi.network/v1/rpc/public","https://optimism-sepolia.gateway.tenderly.co","https://api.zan.top/opt-sepolia"],"chainId":11155420,"network":"optimism-sepolia","chunkSize":100}}'
       DB_URL: 'http://typesense:8108/?apiKey=xyz'
       IPFS_GATEWAY: 'https://ipfs.io/'
       ARWEAVE_GATEWAY: 'https://arweave.net/'
@@ -156,16 +156,16 @@ services:
 #      MAX_REQ_PER_SECOND: ''
 #      MAX_CHECKSUM_LENGTH: ''
 #      LOG_LEVEL: ''
-      HTTP_API_PORT: '$HTTP_API_PORT'
+      HTTP_API_PORT: '8000'
       P2P_ENABLE_IPV4: 'true'
       P2P_ENABLE_IPV6: 'false'
       P2P_ipV4BindAddress: '0.0.0.0'
-      P2P_ipV4BindTcpPort: '$P2P_ipV4BindTcpPort'
-      P2P_ipV4BindWsPort: '$P2P_ipV4BindWsPort'
+      P2P_ipV4BindTcpPort: '9010'
+      P2P_ipV4BindWsPort: '9011'
       P2P_ipV6BindAddress: '::'
-      P2P_ipV6BindTcpPort: '$P2P_ipV6BindTcpPort'
-      P2P_ipV6BindWsPort: '$P2P_ipV6BindWsPort'
-      P2P_ANNOUNCE_ADDRESSES: '$P2P_ANNOUNCE_ADDRESSES'
+      P2P_ipV6BindTcpPort: '9002'
+      P2P_ipV6BindWsPort: '9003'
+      P2P_ANNOUNCE_ADDRESSES: '["/ip4/38.12.2.212/tcp/9010","/ip4/38.12.2.212/ws/tcp/9011"]'
 #      P2P_ANNOUNCE_PRIVATE: ''
 #      P2P_pubsubPeerDiscoveryInterval: ''
 #      P2P_dhtMaxInboundStreams: ''
